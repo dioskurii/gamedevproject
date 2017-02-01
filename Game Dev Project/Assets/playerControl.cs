2e3efaf9f972eaf2ed8237293f489f588993ed2d
Unknown <eh2252@nyu.edu>
2017-02-01 16:27:19 -0500

    Put Unity in the Git Repository

---
+++ Game Dev Project/Assets/playerControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+    public Camera cam;
+    private float maxHeight;
+
+    // Use this for initialization
+    void Start () {
+    if (cam == null) {
+            cam = Camera.main;
+        }
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetHeight = cam.ScreenToWorldPoint(upperCorner);
+        maxHeight = targetHeight.y;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 targetPosition = new Vector3(-2.75f, rawPosition.y, 0);
+        float targetHeight = Mathf.Clamp(targetPosition.y, -maxHeight, maxHeight);
+        targetPosition = new Vector3(targetPosition.x, targetHeight, targetPosition.z);
+        GetComponent<Rigidbody2D>().MovePosition(targetPosition);
+    }
+}
