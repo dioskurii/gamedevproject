379cb2a8dea193fb7b9bb8aaec52a540eff38c99
Unknown <eh2252@nyu.edu>
2017-02-06 15:28:45 -0500

    Finished spawning bullets; need to make them move

---
+++ Game Dev Project/Assets/bulletScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+
+    public Rigidbody2D bulletPrefab;
+    public float attackSpeed = 0.5f;
+    public float coolDown;
+    public float bulletSpeed = 500;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void Fire()
+    {
+        Rigidbody2D bPrefab = (Rigidbody2D)Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+
+        rb.AddForce(Vector2.up * bulletSpeed);
+
+        coolDown = Time.time + attackSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time >= coolDown)
+        {
+            if (Input.GetMouseButton(0)) {
+                Fire();
+            }
+        }
+    }
+}
--- Game Dev Project/Assets/playerControl.cs
+++ Game Dev Project/Assets/playerControl.cs
@@ -1,26 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerControl : MonoBehaviour {
+public class playerControl : MonoBehaviour
+{
     public Camera cam;
     private float maxHeight;
 
+    public bulletScript other;
+
+    // arthurs fault
+    public Rigidbody2D bulletPrefab;
+    public float attackSpeed = 0.5f;
+    public float coolDown;
+    public float bulletSpeed = 500;
+    public Rigidbody2D rb;
+    // all of it
+
+    public void Fire()
+    {
+        Rigidbody2D bPrefab = (Rigidbody2D)Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+
+        rb.AddForce(Vector2.up * bulletSpeed);
+
+        coolDown = Time.time + attackSpeed;
+    }
+
+
+
     // Use this for initialization
-    void Start () {
-    if (cam == null) {
+    void Start()
+    {
+        if (cam == null)
+        {
             cam = Camera.main;
         }
         Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
         Vector3 targetHeight = cam.ScreenToWorldPoint(upperCorner);
         maxHeight = targetHeight.y;
+
+        GetComponent<bulletScript>();
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 targetPosition = new Vector3(-2.75f, rawPosition.y, 0);
         float targetHeight = Mathf.Clamp(targetPosition.y, -maxHeight, maxHeight);
         targetPosition = new Vector3(targetPosition.x, targetHeight, targetPosition.z);
         GetComponent<Rigidbody2D>().MovePosition(targetPosition);
+
+
+
+
+
+        if (Time.time >= coolDown)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Fire();
+            }
+        }
+
+
+
+
     }
 }
